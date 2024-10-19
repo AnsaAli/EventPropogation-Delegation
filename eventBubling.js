f52d0921f6314f01805901aa6f586130a27cceaf
AnsaAli <chinjuansa@example.com>
2024-10-19 11:33:02 +0530

    add event bubling and event capturing

---
+++ eventBubling.js
@@ -0,0 +1,11 @@
+document.getElementById('grandparen').addEventListener('click',()=>{
+    console.log('grand parent clicked')
+},true)
+
+document.getElementById('parent').addEventListener('click',()=>{
+    console.log('parent clicked')
+},true);
+
+document.getElementById('child').addEventListener('click',()=>{
+    console.log('child clicked')
+},true)
