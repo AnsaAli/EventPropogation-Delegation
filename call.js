7603d7b6afd628f9e627dedad258efd167ae56c3
AnsaAli <chinjuansa@example.com>
2024-10-21 12:58:27 +0530

    add apply method

--- call.js
+++ call.js
@@ -1,12 +1,12 @@
-let name ={
+let name1 ={
     firstName : 'Ansa',
     lastName: 'Ali',
    
 }
-let fullName = function(){
-    console.log(this.firstName + " " + this.lastName)
+let fullName = function(hometown,state){
+    console.log(this.firstName + " " + this.lastName + ',' + hometown + ','+state)
 }
-fullName.call(name);
+fullName.call(name1);
 
 let name2={
     firstName: 'Arham',
@@ -14,5 +14,11 @@ let name2={
 
 }
 //function borrowing fron name ti name2
-fullName.call(name2) //here name2 repesents this
+fullName.call(name2) 
+//here name2 repesents this
 
+//if we want to pass the argument in the function using call we need 
+//to pass likename2,'Ernakulam','kerala'
+fullName.call(name2,'Ernakulam','kerala') 
+//but using apply, pass arguments in array
+fullName.apply(name1,['Ernakulam','kerala'])
